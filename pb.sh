ebf02c765b3933aa905fe6749a595ae837b4c886
Abhat27 <advaithbhat9@gmail.com>
2019-01-01 10:55:55 +0000

    PBR ftw

---
+++ pb.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/Abhat27/twrp-8.1.git -b pb device/lenovo/kuntao
+git clone https://github.com/Abhat27/android_kernel_lenovo_msm8953.git -b cm-14.1 kernel/lenovo/msm8953
+git clone https://github.com/Abhat27/android_device_qcom_common.git -b android-8.0 device/qcom/common
